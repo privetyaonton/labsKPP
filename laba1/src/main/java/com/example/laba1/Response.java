134cf24816513f72a52307811fb3c9c9cb0e6da4
privetyaonton <71338577+privetyaonton@users.noreply.github.com>
2022-03-14 10:15:50 +0300

    Add files via upload

---
+++ laba1/src/main/java/com/example/laba1/Response.java
@@ -0,0 +1,20 @@
+package com.example.laba1;
+
+public class Response {
+    private String message;
+
+    public Response() {
+    }
+
+    public Response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
